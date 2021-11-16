a344db2029618f8e277ef43b5b16a500830f2d00
success-master <joybrother2019@gmail.com>
2021-11-16 11:15:36 -0600

    update 11/16/21

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,20 +1,122 @@
 import { React, useState } from 'react';
+import axios from 'axios';
+import Modal from 'react-modal';
 
 export default function Container() {
 
-    const [status, setStatus] = useState(0);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [gender, setGender] = useState(1);
+    const [status, setStatus] = useState(1);
     const [result, setResult] = useState([]);
 
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [selectedId, setSelectedId] = useState(null);
+    const [selectedName, seSselectedName] = useState('');
+    const [selectedEmail, setSelectedEmail] = useState('');
+    const [selectedGender, setSelectedGender] = useState(1);
+    const [selectedStatus, setSelectedStatus] = useState(1);
+
+    const baseUrl = 'https://gorest.co.in/public-api/users';
+
+    const headers = {
+        'Authorization': 'Bearer 9e99287bdd6384a489f60afe7af98550160f0e8cdfd45eb44dc665282be30b27',
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+        'Accept': '*/*'
+    }
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
+        }
+    };
+
     function search() {
-        fetch(`https://gorest.co.in/public-api/users?gender=${parseInt(status) === 0 ? `female` : `male`}`)
+        fetch(`${baseUrl}?gender=${parseInt(gender) === 0 ? `female` : `male`}&status=${parseInt(status) === 0 ? `inactive` : `active`}&name=${name}&email=${email}`)
+            .then(response => response.json())
+            .then(data => {
+                let result = data.data;
+                setResult(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });;
+    }
+
+    function detailResult(e, id) {
+        fetch(`${baseUrl}?id=${id}`)
             .then(response => response.json())
             .then(data => {
                 setResult(data.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    Modal.setAppElement('#root');
+
+    function editData(e, id, name, email, gender, status) {
+        setSelectedId(id);
+        seSselectedName(name);
+        setSelectedEmail(email);
+        setSelectedGender(gender);
+        setSelectedStatus(status);
+        setIsOpen(true);
+    }
+
+    function update() {
+        setIsOpen(false);
+
+        axios.patch(`${baseUrl}/${selectedId}`,
+            {
+                name: selectedName,
+                email: selectedEmail,
+                gender: selectedGender === 0 ? "female" : "male",
+                status: selectedStatus === 0 ? "inactive" : "active",
+            },
+            { headers: headers }
+        )
+            .then(res => {
+                if (res.status === 200) {
+                    detailResult(res, selectedId);
+                }
+            })
+            .catch(err => {
+                console.log('error: ', err);
+            })
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+
+    }
+
+    function deleteData(e, id) {
+        alert('Do you want to delete it?');
+
+        axios.delete(`${baseUrl}/${id}`, { headers })
+            .then(res => {
+                if (res.status === 200) {
+                    search();
+                }
+            })
+            .catch(err => {
+                console.log('error', err);
             });
     }
 
     function clear() {
-        setStatus(0);
+        setName('');
+        setEmail('');
+        setGender(1)
+        setStatus(1);
         setResult([]);
     }
 
@@ -22,83 +124,95 @@ export default function Container() {
         <div className="container">
             <div className="row">
                 <div className="row g-3">
-                    {/* <div className="col-md-3">
-                        <label htmlFor="inputEmail4" className="form-label">Plan Code</label>
-                        <select className="form-select select" aria-label="Disabled select example">
-                            <option value="0">-- select--</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                    </div>
                     <div className="col-md-3">
-                        <label htmlFor="inputPassword4" className="form-label">Plan Name</label>
-                        <select className="form-select select" aria-label="Disabled select example">
-                            <option value="0">-- select--</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                    </div>
-                    <div className="col-md-3">
-                        <label htmlFor="inputPassword4" className="form-label">Client Name</label>
+                        <label htmlFor="name" className="form-label">Name</label>
                         <input type="text" className="form-control" id="name" onChange={(e) => setName(e.target.value)} value={name} />
                     </div>
                     <div className="col-md-3">
-                        <label htmlFor="inputPassword4" className="form-label">Program Name</label>
-                        <select className="form-select select" aria-label="Disabled select example">
-                            <option value="0">-- select--</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="text" className="form-control" id="name" onChange={(e) => setEmail(e.target.value)} value={email} />
                     </div>
-                    <div className="col-md-3 padding-top">
-                        <label htmlFor="inputPassword4" className="form-label">Record</label>
-                        <select className="form-select select">
-                            <option value="0">-- select--</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                    </div> */}
                     <div className="col-md-3 padding-top gender-select">
-                        <label htmlFor="inputPassword4" className="form-label">Gender</label>
-                        <select className="form-select select" onChange={(e) => setStatus(e.target.value)} value={status}>
+                        <label htmlFor="gender" className="form-label">Gender</label>
+                        <select className="form-select select" onChange={(e) => setGender(e.target.value)} value={gender}>
                             <option value="0">female</option>
                             <option value="1">male</option>
                         </select>
                     </div>
+                    <div className="col-md-3 padding-top">
+                        <label htmlFor="status" className="form-label">Status</label>
+                        <select className="form-select select" onChange={(e) => setStatus(e.target.value)} value={status}>
+                            <option value="0">inactive</option>
+                            <option value="1">active</option>
+                        </select>
+                    </div>
 
-                    <div className="col-12 button-right">
+                    <div className="col-12 button-right" style={{ marginTop: "50px", marginBottom: "50px" }}>
                         <button type="submit" className="btn btn-primary orange-button" onClick={() => search()}>Search</button>
                         <button type="submit" className="btn btn-primary orange-button clear-button" onClick={() => clear()}>Clear</button>
                     </div>
 
-                    {result.length > 0 && <div>
+                    {result.length > 0 && <div className="resultTable">
                         <table style={{ width: "100%" }}>
                             <thead>
                                 <tr>
-                                    <th>id</th>
-                                    <th>name</th>
-                                    <th>email</th>
-                                    <th>gender</th>
-                                    <th>status</th>
+                                    <th>ID</th>
+                                    <th>NAME</th>
+                                    <th>EMAIL</th>
+                                    <th>GENDER</th>
+                                    <th>STATUS</th>
+                                    <th>EDIT</th>
+                                    <th>DELETE</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {result.map((person, index) => (
                                     <tr key={index}>
-                                        <td>{person.id}</td>
+                                        <td><a href="#!" onClick={(e) => detailResult(e, person.id)}>{person.id}</a></td>
                                         <td>{person.name}</td>
                                         <td>{person.email}</td>
                                         <td>{person.gender}</td>
                                         <td>{person.status}</td>
+                                        <td><a href="#!" onClick={(e) => editData(e, person.id, person.name, person.email, person.gender === 'male' ? 1 : 0, person.status === 'active' ? 1 : 0)}>Edit</a></td>
+                                        <td><a href="#!" onClick={(e) => deleteData(e, person.id)}>Delete</a></td>
                                     </tr>
                                 ))}
                             </tbody>
                         </table>
                     </div>}
+                    <Modal
+                        isOpen={modalIsOpen}
+                        onRequestClose={closeModal}
+                        style={customStyles}
+                        contentLabel="Edit Modal"
+                    >
+                        <h2 style={{ textAlign: "center" }}>Edit Modal</h2>
+                        <div style={{ display: "flex" }}>
+                            <div className="col-md-3">
+                                <label htmlFor="name" className="form-label">Name</label>
+                                <input type="text" className="form-control" id="name" onChange={(e) => seSselectedName(e.target.value)} value={selectedName} />
+                            </div>
+                            <div className="col-md-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input type="text" className="form-control" id="name" onChange={(e) => setSelectedEmail(e.target.value)} value={selectedEmail} />
+                            </div>
+                            <div className="col-md-3 padding-top gender-select">
+                                <label htmlFor="gender" className="form-label">Gender</label>
+                                <select className="form-select select" onChange={(e) => setSelectedGender(e.target.value)} value={selectedGender}>
+                                    <option value="0">female</option>
+                                    <option value="1">male</option>
+                                </select>
+                            </div>
+                            <div className="col-md-3 padding-top">
+                                <label htmlFor="status" className="form-label">Status</label>
+                                <select className="form-select select" onChange={(e) => setSelectedStatus(e.target.value)} value={selectedStatus}>
+                                    <option value="0">inactive</option>
+                                    <option value="1">active</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button onClick={() => update()} style={{ float: "right", marginTop: "20px", marginRight: "15px" }}>Update</button>
+                    </Modal>
                 </div>
             </div>
         </div>
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,12 +4,12 @@ export default function Wrapper() {
             <nav aria-label="breadcrumb">
                 <ol className="breadcrumb" style={{ padding: "16px" }}>
                     <li className="breadcrumb-item"><a href="/#">Warranty Component Administration </a></li>
-                    <li className="breadcrumb-item active" aria-current="page">Plan</li>
+                    <li className="breadcrumb-item active" aria-current="page">User</li>
                 </ol>
             </nav>
             <div className="container welcome-container">
-                <div className="welcome">Welcome Chethan</div>
-                <h4 className="search">Search Plan</h4>
+                <div className="welcome">Welcome!</div>
+                <h4 className="search">Search User</h4>
             </div>
         </div>
     );
